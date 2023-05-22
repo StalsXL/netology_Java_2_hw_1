712e58427516493b10b9047c81625cc30e32a40c
Denis Fedosov <stalsxl@gmail.com>
2023-05-22 19:02:28 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 14000;
+        int bonus = 20;
+        int result = price / bonus;
+        System.out.println("Начислено " + result + " бонусов");
+    }
+}
+
